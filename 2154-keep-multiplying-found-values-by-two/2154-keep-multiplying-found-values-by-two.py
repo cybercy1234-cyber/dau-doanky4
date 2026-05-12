3006681a078e78ccf69979d6883bfcdc0a603eed
cybercy1234-cyber <cybercy1234@gmail.com>
2026-05-12 12:31:23 +0700

    Time: 3 ms (25.86%), Space: 12.6 MB (28.82%) - LeetHub

---
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def findFinalValue(self, nums, original):
+        nums = set(nums)
+
+        while original in nums:
+            original *= 2
+
+        return original
